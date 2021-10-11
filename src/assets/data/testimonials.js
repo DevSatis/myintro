3b0eb00e259b4bfd397bb5a89fc6822fabdd47e9
Satish Kumar <sksatishk9@gmail.com>
2021-10-11 10:26:35 +0530

    Myintro Commit

---
+++ src/assets/data/testimonials.js
@@ -0,0 +1,27 @@
+const testimonials = [
+    {
+      id: 1,
+      name: 'Satish Kumar',
+      title: 'CEO',
+      org: 'Akhilen ',
+      desc:
+        "Satish is a person of commitment. I am really good at what I'm doing. Especially my unique point of view of designing."
+    },
+    {
+      id: 2,
+      name: 'Vanshika',
+      title: 'Co-founder',
+      org: 'Akhilen',
+      desc:
+        'Really amazing communication skills. Always understand what I am trying to achieve. Also, her work is really amazing. Do really high-quality work.',
+    },
+    {
+      id: 3,
+      name: 'Prasoon',
+      title: 'Marketing Head',
+      org: 'Akhilen',
+      desc:
+        'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
+    },
+  ];
+  export default testimonials;
